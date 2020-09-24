f6b353e35d2ca87dadd7ae408f07997f43855ca5
Mikey <richardsm3@my.lowercolumbia.edu>
2020-09-24 10:56:40 -0700

    Added seperate token file, added detection for unauthorized errors, made ping only occur after two errors in a row

--- bot.js
+++ bot.js
@@ -1,19 +1,31 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
+var errorCount = 0
 
-//TODO: Add scraper to fetch updated auth token at the start of each week
 const headers = {
-	"Authorization": process.env.token,
+	"Authorization": "",
 	"Origin": "https://www.pokemoncenter.com"
 }
 
-//Once every 5 minutes, call fetchStatusFile
-setInterval(fetchStatusFile, 300000);
-fetchStatusFile()
+//TODO: Add scraper to fetch updated auth token at the start of each week
+//Read token.txt and pass contents to fetchStatusFile
+function fetchToken() {
+	fs.readFile("token.txt", "utf8", fetchStatusFile);
+}
+
+//Once every 5 minutes, call fetchToken
+setInterval(fetchToken, 300000);
+fetchToken()
 
 //Read status.json and pass contents to parseStatusFile
-function fetchStatusFile() {
-	fs.readFile("status.json", 'utf8', parseStatusFile);
+function fetchStatusFile(err, data) {
+	if(err) {
+		console.error(err);
+	}
+	else {
+		headers["Authorization"] = data;
+		fs.readFile("status.json", 'utf8', parseStatusFile);
+	}
 }
 
 //Parse the JSON file into an array and pass it to updateItems
@@ -55,16 +67,17 @@ function handleFileError(err) {
 
 //Get the status of an item from PokeCenter by calling getStatus and update the status field
 async function updateItemStatus(item) {
-	item.status = await getStatus(item.url);
+	item.status = await getStatus(item.url, item.status);
 	return item;
 }
 
 //Make a request for the url specified, parse the page data with parsePage, and update the status accordingly
-async function getStatus(url) {
+async function getStatus(url, oldStatus) {
 	try {
 		const productID = url.substring(38, 47);
 		const res = await fetch("https://www.pokemoncenter.com/tpci-ecommweb-api/product?format=zoom.nodatalinks", {method: "post", headers: headers, body: '{"productSku":"' + productID + '"}'});
 		if(res.ok) {
+			errorCount = 0;
 			const body = await res.text();
 			const index = body.indexOf("state");
 			const state = body.substring(index + 6, index + 15);
@@ -76,8 +89,19 @@ async function getStatus(url) {
 			}
 		}
 		else {
-			console.error("Could not fetch " + url + " - " + res.statusText);
-			return "unknown";
+			console.error("Could not fetch " + url + " - " + res.status + " - " + res.statusText);
+			if(res.status == 401) {
+				return "invalid_token";
+			}
+			else {
+				errorCount++;
+				if(errorCount > 1) {
+					return "unknown";
+				}
+				else {
+					return oldStatus;
+				}
+			}
 		}
 	}
 	catch(err) {
